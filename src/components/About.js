e2c4689d06052bcf63d2851c07d6deeba96ff9d4
Hunter Wallen <hunterwallen67@gmail.com>
2021-02-13 12:57:40 -0700

    styled about me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,27 @@
 import React from "react";
 import Avatar from "./assets/avatar.jpg";
+import aboutStyles from '../styles/about.css'
 
 export default function About(props) {
 	return (
 		<React.Fragment>
-			<h1 style={{ textAlign: "center" }}>
-				I'm a Software Engineer! I build applications
-				<br /> for the web, focusing on scalable code
-				<br /> and sharp User Interfaces and stuff!
-				<br /> When I'm not working, I'm likely at
-				<br /> the beach with my girlfriend and our dog.
-			</h1>
-			<img
-				src={Avatar}
-				alt={"avatar"}
-				style={{
-					maxWidth: "15rem",
-					borderRadius: "50%",
-					display: "block",
-					marginLeft: "auto",
-					marginRight: "auto",
-				}}
-			/>
+			<div className="fullAboutPage">
+				<div className="aboutContent">
+				<h2 className="nameTitle">A Little About Me</h2>
+				<img
+					src={Avatar}
+					alt={"avatar"}
+					className="aboutPic"
+				/>
+					<h1 style={{ textAlign: "center" }}>
+					I am a full-stack software engineer that takes pride in delivering clean, scalable solutions. There is nothing in life that brings me more joy than the pursuit of knowledge. I consider myself to forever be a student and find great satisfaction in learning new things.
+					<br/> 
+					<br/>Outside of coding and developing software, I love the great outdoors. There's nothing like taking in a beautiful sunset from the top of a mountain to refresh the soul.
+					<br/>
+					<br/>If you have an opportunity you think I would be a good fit for, I would love to chat!
+					</h1>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,11 +5,10 @@ export default function About(props) {
 	return (
 		<React.Fragment>
     <div style={{ textAlign: "center" }}>
-      <h1>Contact Me:</h1>
-      <h2>Leland Shirley</h2>
-      <h3>leland.shirley@generalassemb.ly</h3>
-      <h3>Los Angeles & Pittsburgh</h3>
-      <h3>(555) 555-5555</h3>
+      <h1>Want to say hello?</h1>
+      <h2>Hunter Wallen</h2>
+      <h3>hunterwallen67@gmail.com</h3>
+      <h3>Denver, CO</h3>
     </div>
 		</React.Fragment>
 	);
